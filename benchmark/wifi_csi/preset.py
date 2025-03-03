63643dd35123cdea90b6d0de0ab9f89a29e8f129
amirmhd <mdi.amirhossein@gmail.com>
2025-03-03 10:32:25 -0800

    .gitignore

--- benchmark/wifi_csi/preset.py
+++ benchmark/wifi_csi/preset.py
@@ -17,11 +17,11 @@ preset = {
     "task": "activity",                                 # "identity", "activity", "location"
     #
     ## number of repeated experiments
-    "repeat": 5,
+    "repeat": 8,
     ## path of data
     "path": {
-        "data_x": "/local/data0/amir/PUBLIC_DATASET/wimans_dataset/wifi_csi/amp",               # directory of CSI amplitude files
-        "data_y": "/local/data0/amir/PUBLIC_DATASET/wimans_dataset/annotation.csv",             # path of annotation file
+        "data_x": "/home/amirmhd/projects/def-hinat/amirmhd/multi_modal_CSI/dataset/wifi_csi/amp",               # directory of CSI amplitude files
+        "data_y": "/home/amirmhd/projects/def-hinat/amirmhd/multi_modal_CSI/dataset/annotation.csv",             # path of annotation file
         "save": "results/result.json"                           # path to save results
     },
     #
@@ -29,7 +29,7 @@ preset = {
     "data": {
         "num_users": ["0","1", "2", "3", "4", "5"] ,   # select number(s) of users, (e.g., ["0", "1"], ["2", "3", "4", "5"])
         "wifi_band": ["5"],                           # select WiFi band(s) (e.g., ["2.4"], ["5"], ["2.4", "5"])
-        "environment": ["empty_room"],                   # select environment(s) (e.g., ["classroom"], ["meeting_room"], ["empty_room"])
+        "environment": ["classroom"],                   # select environment(s) (e.g., ["classroom"], ["meeting_room"], ["empty_room"])
         "length": 3000,                                 # default length of CSI
     },
     "data_band2": {
