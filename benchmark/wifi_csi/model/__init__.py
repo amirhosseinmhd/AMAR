331bdf0cb062234cffd8c1440c4894930e31073d
amirmhd <mdi.amirhossein@gmail.com>
2025-11-11 22:13:41 -0500

    Remove unused imports and update data paths in preset configuration

--- benchmark/wifi_csi/model/__init__.py
+++ benchmark/wifi_csi/model/__init__.py
@@ -12,19 +12,15 @@ from .cnn_2d import run_cnn_2d
 from .cnn_lstm import run_cnn_lstm
 from .ablstm import run_ablstm
 from .that_count_pred import run_that_count_pred
-from .SSL_model import run_ssl
 from .that import run_that
 from .dual_band import run_dual_band
 from .that_multi_head import run_that_multihead
 from .that_count_pred_withSoftMaxConstraint import run_that_count_pred_contrained
 from .that_encoder import run_that_decoder
 from .detr import run_that_detr
-from .joint_location_detr import run_joint_detr
 from .crowd_counting_THAT import run_crowd_counting_THAT
 from .multi_senseX import run_multi_senseX
 from .multi_user import run_multi_user
-from .JEPA_Supervised import run_JEPA_hyb
-from .JEPA import run_JEPA
 from .detr_vq import run_that_detrRVQ
 
 #
@@ -37,17 +33,13 @@ __all__ = ["run_strf",
            "run_cnn_lstm",
            "run_ablstm",
            "run_that",
-           "run_ssl",
            "run_dual_band",
            "run_that_multihead",
            "run_that_count_pred",
            "run_that_count_pred_contrained",
            "run_that_decoder",
            "run_that_detr",
-            "run_joint_detr",
            "run_crowd_counting_THAT",
            "run_multi_senseX",
            "run_multi_user",
-           "run_JEPA_hyb",
-           "run_JEPA",
            "run_that_detrRVQ"]
